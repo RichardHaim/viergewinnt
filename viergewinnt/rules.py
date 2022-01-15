7cb095a341e1d3af24651fd1a3411c17bb57912c
Dorian Pfeiler <dorian.pfeiler@campus02.at>
2022-01-15 17:11:06 +0100

    Prints out the rules of the game with function: def rules_set

---
+++ viergewinnt/rules.py
@@ -0,0 +1,13 @@
+def rules_set():
+    rules = ('''
+RULES OF THE GAME:
+The game is played on a vertical board which has seven columns and six rows. 
+The aim for both players is to make a straight line of four OWN pieces (in our version of the game the pieces will be
+"X" and "O"). The Line can be vertical, horizontal or diagonal. Moves are made alternatively, one by turn.
+If none reaches to put 4 pieces in a line, its a draw. 
+''')
+
+    return print (rules)
+
+if __name__ == '__main__':
+    rules_set()
